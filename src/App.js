86e083125c1e209295f3fa2269e9ac53f99d0c14
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-09 09:52:46 -0500

    File Structure

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ class App extends Component {
 
   render() {
     return (
-      <>
-      </>
+      <div className="App">
+        <header className="App-header">Pokemon Index</header>
+      </div>
     )
   }
 
