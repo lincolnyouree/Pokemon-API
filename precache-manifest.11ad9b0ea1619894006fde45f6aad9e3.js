aa612db43df3245b58e989ba650e9a5eeb051ce6
Lincoln Youree <lincolnyouree@Lincolns-Air.attlocal.net>
2020-04-14 11:01:44 -0500

    Updates

---
+++ precache-manifest.11ad9b0ea1619894006fde45f6aad9e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4253ed74b49714444ab8a3aeaf8935e0",
+    "url": "/Pokemon-Index/index.html"
+  },
+  {
+    "revision": "b4923317cb5806d638e4",
+    "url": "/Pokemon-Index/static/css/main.27dc19e9.chunk.css"
+  },
+  {
+    "revision": "44b0bb2dac831f060ed5",
+    "url": "/Pokemon-Index/static/js/2.9c793053.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Pokemon-Index/static/js/2.9c793053.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4923317cb5806d638e4",
+    "url": "/Pokemon-Index/static/js/main.23ee2f83.chunk.js"
+  },
+  {
+    "revision": "61de677605c19117a4f9",
+    "url": "/Pokemon-Index/static/js/runtime-main.714917e2.js"
+  }
+]);
